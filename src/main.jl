00626ed85f011df05a010cb5e24f86f7486c15cc
Amanda Nicotina <amandanicotina@gmal.com>
2024-03-25 17:58:43 +0100

    added .Conda to gitignore

--- src/main.jl
+++ src/main.jl
@@ -6,16 +6,17 @@ using ParameterSchedulers
 M0 = [0.0; 0.0; 1.0];
 T1 = [0.900, 0.070];
 T2 = [0.300, 0.030];
-target = ["min", "max"];
+target = ["max", "min"];
 label = ["T1-900ms", "T1-70ms"];
 # T1 = [1.830, 0.622, 2.430];
 # T2 = [0.184, 0.092, 0.132];
 # target = ["max", "min", "min"];# label = ["GD", "Yolk", "White"];
 
 
-t_c = 0.5;
+t_c = 1.0;
 N   = 600;
 B0  = [0.0];
+#B0  = [-25.0, 0.0, 25.0];
 # B0  = [-100.0, -75.0, -50.0, -25.0, 0.0, 25.0, 50.0, 75.0, 100.0]; # [Hz]
 Bz  = zeros(1,N);
 B1  = 1.0; # [Hz]
@@ -27,10 +28,10 @@ B1y = B1*initial_field_spline(N, t_c, B1)';
 (spins, field_init) = normalization(M0, T1, T2, B0, target, label, t_c, B1x, B1y, ΔB1, Bz);
 
 ##### OPTIMIZE #####
-max_iter = 500
+max_iter = 2000
 lr_scheduler = Poly(start=1e-1, degree=2, max_iter=max_iter+1) 
 opt_params   = OptimizationParams(N, max_iter, cost_saturation_contrast, [true true false]);
-grape_output = @time grape(opt_params, field_init, spins, lr_scheduler; max_iter = max_iter, ϵ=1e-3); 
+grape_output = @time grape(opt_params, field_init, spins, lr_scheduler; max_iter = max_iter, ϵ=1e-2); 
 
 ##### PLOTS #####
 plot_magnetization_Mz_Mxy(grape_output.isochromats)
