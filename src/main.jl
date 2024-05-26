7ad66fb0d8bbbe4888c8087c5668686811613ccc
Amanda Nicotina <amandanicotina@gmal.com>
2024-05-26 11:13:34 +0200

    .

--- src/main.jl
+++ src/main.jl
@@ -4,38 +4,34 @@ using ParameterSchedulers
 
 ##### INITIALIZATION #####
 # Parameters
-N  = 1500     # Number of points
-α  = 2π/9    # Flip angle in radians
-Δϕ = π       # Phase cycling
-TR = 4.28e-3 # Repetition time in seconds
-TE = 2.19e-3 # Echo time in seconds       
-Tc = 0.5     # Control time in seconds
+N  = 1000     # Number of points
+Tc = 1.0     # Control time in seconds
 
 # RFs
 B1x = spline_RF(N, Tc)'
 B1y = spline_RF(N, Tc)'
-ΔB1, Bz = [1.0, 0.9, 0.8], zeros(1,N)
+ΔB1, Bz = [1.0], zeros(1,N)
 
 # Spin parameters
-M0 = [0.0, 0.0, 1.0] # Initial magnetization vector
-T1 = [1.0, 0.08]     # Longitudinal relaxation time
-T2 = [0.6, 0.03]     # Transverse relaxation time
-B0 = 1/TR           # [Hz]
-B0_vals = [-B0, -B0/2, 0.0, B0/2, B0] 
-target  = ["min", "max"]
-label   = ["T1-$(Int(T1[1]*1e3))ms", "T1-$(Int(T1[2]*1e3))ms"]
+# T1 = [1.0, 0.08]     # Longitudinal relaxation time
+# T2 = [0.6, 0.03]     # Transverse relaxation time
+# target  = ["min", "max"]
+# label   = ["T1-$(Int(T1[1]*1e3))ms", "T1-$(Int(T1[2]*1e3))ms"]
 
-# T1 = [1.830, 0.622, 2.430];
-# T2 = [0.184, 0.092, 0.132];
-# label = ["GD", "Yolk", "White"];
-# target = ["max", "min", "min"];
+M0 = [0.0, 0.0, 1.0] 
+T1 = [0.8, 0.4]#, 2.430]
+T2 = [0.6, 0.2]#, 0.132]
+label = ["T1-$(Int(T1[1]*1e3))ms", "T1-$(Int(T1[2]*1e3))ms"]#, "White"]
+target = ["min", "max"]#, "min"]
+B0 = 50.0
+B0_vals = [-B0, -B0/2, 0.0, B0/2, B0] 
 
 # Spin and RF objects
 control_field = ControlField(B1x, B1y, 1.0, Bz, Tc)
 spins  = GrapeMR.Spin(M0, T1, T2, B0_vals, ΔB1, target, label)
 
 ##### OPTIMIZE #####
-max_iter     = 2000
+max_iter     = 15000
 lr_scheduler = Poly(start=1e-1, degree=2, max_iter=max_iter+1) 
 opt_params   = OptimizationParams(N, max_iter, :saturation_contrast, [true true false]);
 grape_output = @time grape(opt_params, control_field, spins, lr_scheduler; max_iter = max_iter, ϵ=1e-2); 
